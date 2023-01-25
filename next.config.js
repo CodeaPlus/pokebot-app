0ce120f71beac86029877062e630fe7623948f31
Diego Becerra <dabc2703@gmail.com>
2023-01-25 13:42:01 -0500

    Remove BOT TOKEN from next.config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    DISCORD_BOT_TOKEN: "MTA2MzU4ODMzNzg5NTYxNjYyMg.G4L7cK.vvkDhq3JDiZeSv12KLqfyBO2m67U1aspa52JkY",
-    BASR_API_URL: "http://localhost:3000/api",
-  },
   images: {
     domains: ['media.discordapp.net', 'cdn.discordapp.com', 'raw.githubusercontent.com']
   }
